641238f90d9ff45bfd52592dfefffbb0bc203826
CaioSimioni <83130766+CaioSimioni@users.noreply.github.com>
2021-04-29 11:55:05 -0300

    Add files via upload

---
+++ Action.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+"""
+[pt-br]
+autor: Caio Ribeiro Simioni
+
+-Referências:
+Como mover, copiar e apagar arquivos com Python - Aula 32 = https://www.youtube.com/watch?v=m8orGLxO03s
+
+-Objetivo:
+Conseguir mover arquivos, substituindo-os através da referência do caminho das pastas.
+
+"""
+print('')
+caminhoOrigem  = r'<caminho das pastas>'  # Arquivo que será copiado
+caminhoDestino = r'<caminho das pastas>' # Arquivo que será substituído
+
+try:
+    os.remove(caminhoDestino+r'\ABC.txt')
+    shutil.copy(caminhoOrigem+r'\ABC.txt', caminhoDestino+r'\ABC.txt')
+    print('Funcionou!')
+except NameError:
+    print('Não funcionou!')
+    raise
+
+os.system('PAUSE')
